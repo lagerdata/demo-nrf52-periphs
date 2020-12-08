6ea9ab78d229455f21daee90fd0dbadce2c88d64
Evan Haas <evan@lagerdata.com>
2020-12-08 11:00:09 -0800

    wip

--- modules/shell/shell.c
+++ modules/shell/shell.c
@@ -27,7 +27,7 @@ static void polling_timer_event_handler(nrf_timer_event_t event_type, void * p_c
 //-------------------------EXPORTED VARIABLES ------------------------------
 
 //-------------------------GLOBAL VARIABLES---------------------------------
-static nrfx_uart_t g_uart0 = NRFX_UART_INSTANCE(0);
+// static nrfx_uart_t g_uart0 = NRFX_UART_INSTANCE(0);
 const char c_shell_context[] = "SHELL";
 static uint8_t g_data_buf[32] = {0};
 const char c_menu[] = "\
@@ -52,53 +52,141 @@ static nrfx_timer_t g_polling = NRFX_TIMER_INSTANCE(2);
 static bool g_streaming_imu = false;
 static bool g_raw = false;
 //-------------------------EXPORTED FUNCTIONS-------------------------------
-void shell_init(void)
-{
-
-    nrfx_uart_config_t uart_cfg = {
-                                   .pseltxd = 6,
-                                   .pselrxd = 8,
-                                   .pselcts = NRF_UART_PSEL_DISCONNECTED,
-                                   .pselrts = NRF_UART_PSEL_DISCONNECTED,
-                                   .p_context = (void *)c_shell_context,
-                                   .hwfc = NRF_UART_HWFC_ENABLED,
-                                   .parity = NRF_UART_PARITY_EXCLUDED,
-                                   .baudrate = NRF_UART_BAUDRATE_115200,
-                                   .interrupt_priority = 7};
-    nrfx_uart_init(&g_uart0, &uart_cfg, uart_event_handler);
-    nrfx_uart_rx_enable(&g_uart0);
-    nrfx_uart_rx(&g_uart0, &g_data_buf[0], 1);
-
-    nrfx_timer_config_t cfg = {
-                               .frequency = NRF_TIMER_FREQ_16MHz,
-                               .mode = NRF_TIMER_MODE_TIMER,
-                               .bit_width = NRF_TIMER_BIT_WIDTH_32,
-                               .interrupt_priority = 6,
-                               .p_context = NULL};
-    nrfx_timer_init(&g_polling, &cfg, polling_timer_event_handler);
-    uint32_t cc_time = nrfx_timer_ms_to_ticks(&g_polling, 10);
-    nrfx_timer_extended_compare(&g_polling, NRF_TIMER_CC_CHANNEL0, cc_time, NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK, true);
-    nrfx_timer_enable(&g_polling);
-
-
-}
-
-
-//-------------------------LOCAL FUNCTIONS----------------------------------
-// static void uart_event_handler(nrfx_uart_event_t const * p_event, void * p_context)
+// void shell_init(void)
 // {
-//     switch(p_event->type){
-//         case NRFX_UART_EVT_RX_DONE:{
-//             handle_rx_bytes((nrfx_uart_xfer_evt_t *)&p_event->data.rxtx);
-//             nrfx_uart_rx(&g_uart0, &g_data_buf[0], 1);
+
+//     nrfx_uart_config_t uart_cfg = {
+//                                    .pseltxd = 6,
+//                                    .pselrxd = 8,
+//                                    .pselcts = NRF_UART_PSEL_DISCONNECTED,
+//                                    .pselrts = NRF_UART_PSEL_DISCONNECTED,
+//                                    .p_context = (void *)c_shell_context,
+//                                    .hwfc = NRF_UART_HWFC_ENABLED,
+//                                    .parity = NRF_UART_PARITY_EXCLUDED,
+//                                    .baudrate = NRF_UART_BAUDRATE_115200,
+//                                    .interrupt_priority = 7};
+//     nrfx_uart_init(&g_uart0, &uart_cfg, uart_event_handler);
+//     nrfx_uart_rx_enable(&g_uart0);
+//     nrfx_uart_rx(&g_uart0, &g_data_buf[0], 1);
+
+//     nrfx_timer_config_t cfg = {
+//                                .frequency = NRF_TIMER_FREQ_16MHz,
+//                                .mode = NRF_TIMER_MODE_TIMER,
+//                                .bit_width = NRF_TIMER_BIT_WIDTH_32,
+//                                .interrupt_priority = 6,
+//                                .p_context = NULL};
+//     nrfx_timer_init(&g_polling, &cfg, polling_timer_event_handler);
+//     uint32_t cc_time = nrfx_timer_ms_to_ticks(&g_polling, 10);
+//     nrfx_timer_extended_compare(&g_polling, NRF_TIMER_CC_CHANNEL0, cc_time, NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK, true);
+//     nrfx_timer_enable(&g_polling);
+
+
+// }
+
+// void handle_rx_bytes(nrfx_uart_xfer_evt_t * p_rxtx)
+// {
+    // switch(*p_rxtx->p_data){
+    //     case 'h':{
+    //         nrfx_uart_tx(&g_uart0, (uint8_t const *)"Hello World\r\n", sizeof("Hello World\r\n"));
+    //         break;
+    //     }
+    //     case 's': {
+    //         int32_t sum;
+    //         sum = add(40, 2);
+    //         char buf[32];
+    //         sprintf((char *)buf, "Sum: %ld\r\n", sum);
+    //         nrfx_uart_tx(&g_uart0, (uint8_t const *)buf, strlen(buf) + 1);
+    //         break;
+    //     }
+    //     case 'i':{
+    //         if(true == g_streaming_imu){
+    //             g_streaming_imu = false;
+    //         }else{
+    //             trigger_imu_stream();
+    //             g_streaming_imu = true;
+    //             g_raw = false;
+    //         }
+    //         break;
+    //     }
+
+    //     case 'r':{
+    //         if(true == g_streaming_imu){
+    //             g_streaming_imu = false;
+    //         }else{
+    //             trigger_imu_stream();
+    //             g_streaming_imu = true;
+    //             g_raw = true;
+    //         }
+    //         break;
+    //     }
+
+    //     case 'l':{
+    //         const char led_on_msg[] = "Turning on LED 0.\r\n";
+    //         nrfx_uart_tx(&g_uart0, (uint8_t const *)led_on_msg, sizeof(led_on_msg));
+    //         ledctrl_onoff(true, 0);
+
+    //         break;
+    //     }
+
+    //     case 'k':{
+    //         const char led_on_msg[] = "Turning off LED 0.\r\n";
+    //         nrfx_uart_tx(&g_uart0, (uint8_t const *)led_on_msg, sizeof(led_on_msg));
+    //         ledctrl_onoff(false, 0);
+
+    //         break;
+    //     }
+
+    //     case 't':{
+    //         if(false ==ledctrl_is_led_num_on(0)){
+    //             const char led_on_msg[] = "Turning on LED 0.\r\n";
+    //             nrfx_uart_tx(&g_uart0, (uint8_t const *)led_on_msg, sizeof(led_on_msg));
+    //             ledctrl_onoff(true, 0);
+    //         }else{
+    //             const char led_off_msg[] = "Turning off LED 0.\r\n";
+    //             nrfx_uart_tx(&g_uart0, (uint8_t const *)led_off_msg, sizeof(led_off_msg));
+    //             ledctrl_onoff(false, 0);
+    //         }
+    //         break;
+    //     }
+
+    //     default:{
+    //         nrfx_uart_tx(&g_uart0, (uint8_t const *)c_menu, sizeof(c_menu));
+    //         break;
+    //     }
+    // }
+// }
+
+
+// static void polling_timer_event_handler(nrf_timer_event_t event_type, void * p_context)
+// {
+//     switch(event_type){
+//         case NRF_TIMER_EVENT_COMPARE0:{
+//             if(true == g_streaming_imu){//check if we're streaming IMU data
+//                 uint8_t accel_buf[6];
+//                 mpu9250_drv_read_accel(&accel_buf);
+//                 if(true == g_raw){
+//                     nrfx_uart_tx(&g_uart0, (uint8_t const *)accel_buf, sizeof(accel_buf));
+//                 }else{
+//                     MPU9250_accel_val accel_val;
+//                     mpu9250_drv_process_raw_accel(&accel_val, &accel_buf);
+//                     char buf[64];
+//                     size_t len = sprintf((char *)buf, "ACCEL(x,y,z):%f,%f,%f\r\n",accel_val.x, accel_val.y, accel_val.z);
+//                     nrfx_uart_tx(&g_uart0, (uint8_t const *)buf, len);
+//                 }
+
+//             }
 //             break;
 //         }
 
-//         case NRFX_UART_EVT_TX_DONE:{
+//         case NRF_TIMER_EVENT_COMPARE1:{
 //             break;
 //         }
 
-//         case NRFX_UART_EVT_ERROR:{
+//         case NRF_TIMER_EVENT_COMPARE2:{
+//             break;
+//         }
+
+//         case NRF_TIMER_EVENT_COMPARE3:{
 //             break;
 //         }
 
@@ -106,118 +194,5 @@ void shell_init(void)
 //             break;
 //         }
 //     }
+
 // }
-
-void handle_rx_bytes(nrfx_uart_xfer_evt_t * p_rxtx)
-{
-    switch(*p_rxtx->p_data){
-        case 'h':{
-            nrfx_uart_tx(&g_uart0, (uint8_t const *)"Hello World\r\n", sizeof("Hello World\r\n"));
-            break;
-        }
-        case 's': {
-            int32_t sum;
-            sum = add(40, 2);
-            char buf[32];
-            sprintf((char *)buf, "Sum: %ld\r\n", sum);
-            nrfx_uart_tx(&g_uart0, (uint8_t const *)buf, strlen(buf) + 1);
-            break;
-        }
-        case 'i':{
-            if(true == g_streaming_imu){
-                g_streaming_imu = false;
-            }else{
-                trigger_imu_stream();
-                g_streaming_imu = true;
-                g_raw = false;
-            }
-            break;
-        }
-
-        case 'r':{
-            if(true == g_streaming_imu){
-                g_streaming_imu = false;
-            }else{
-                trigger_imu_stream();
-                g_streaming_imu = true;
-                g_raw = true;
-            }
-            break;
-        }
-
-        case 'l':{
-            const char led_on_msg[] = "Turning on LED 0.\r\n";
-            nrfx_uart_tx(&g_uart0, (uint8_t const *)led_on_msg, sizeof(led_on_msg));
-            ledctrl_onoff(true, 0);
-
-            break;
-        }
-
-        case 'k':{
-            const char led_on_msg[] = "Turning off LED 0.\r\n";
-            nrfx_uart_tx(&g_uart0, (uint8_t const *)led_on_msg, sizeof(led_on_msg));
-            ledctrl_onoff(false, 0);
-
-            break;
-        }
-
-        case 't':{
-            if(false ==ledctrl_is_led_num_on(0)){
-                const char led_on_msg[] = "Turning on LED 0.\r\n";
-                nrfx_uart_tx(&g_uart0, (uint8_t const *)led_on_msg, sizeof(led_on_msg));
-                ledctrl_onoff(true, 0);
-            }else{
-                const char led_off_msg[] = "Turning off LED 0.\r\n";
-                nrfx_uart_tx(&g_uart0, (uint8_t const *)led_off_msg, sizeof(led_off_msg));
-                ledctrl_onoff(false, 0);
-            }
-            break;
-        }
-
-        default:{
-            nrfx_uart_tx(&g_uart0, (uint8_t const *)c_menu, sizeof(c_menu));
-            break;
-        }
-    }
-}
-
-
-static void polling_timer_event_handler(nrf_timer_event_t event_type, void * p_context)
-{
-    switch(event_type){
-        case NRF_TIMER_EVENT_COMPARE0:{
-            if(true == g_streaming_imu){//check if we're streaming IMU data
-                uint8_t accel_buf[6];
-                mpu9250_drv_read_accel(&accel_buf);
-                if(true == g_raw){
-                    nrfx_uart_tx(&g_uart0, (uint8_t const *)accel_buf, sizeof(accel_buf));
-                }else{
-                    MPU9250_accel_val accel_val;
-                    mpu9250_drv_process_raw_accel(&accel_val, &accel_buf);
-                    char buf[64];
-                    size_t len = sprintf((char *)buf, "ACCEL(x,y,z):%f,%f,%f\r\n",accel_val.x, accel_val.y, accel_val.z);
-                    nrfx_uart_tx(&g_uart0, (uint8_t const *)buf, len);
-                }
-
-            }
-            break;
-        }
-
-        case NRF_TIMER_EVENT_COMPARE1:{
-            break;
-        }
-
-        case NRF_TIMER_EVENT_COMPARE2:{
-            break;
-        }
-
-        case NRF_TIMER_EVENT_COMPARE3:{
-            break;
-        }
-
-        default:{
-            break;
-        }
-    }
-
-}
--- zig/src/shell.zig
+++ zig/src/shell.zig
@@ -12,12 +12,12 @@ const c = @cImport({
     @cInclude("/github/workspace/modules/imu/mpu9250.h");
 });
 
-const g_uart0 = c.nrfx_uart_t {
-    .p_reg        = c.NRF_UART0,
+threadlocal var g_uart0 = c.nrfx_uart_t{
+    .p_reg = c.NRF_UART0,
     .drv_inst_idx = c.NRFX_UART0_INST_IDX,
 };
 
-const g_polling = c.nrfx_timer_t {
+threadlocal var g_polling = c.nrfx_timer_t{
     .p_reg = c.NRF_TIMER2,
     .instance_id = c.NRFX_TIMER2_INST_IDX,
     .cc_channel_count = c.TIMER2_CC_NUM,
@@ -37,13 +37,10 @@ export fn trigger_imu_stream() void {
     _ = c.mpu9250_start_measure(MPU9250_BIT_GYRO_FS_SEL_1000DPS, MPU9250_BIT_ACCEL_FS_SEL_8G, MPU9250_BIT_DLPF_CFG_250HZ, MPU9250_BIT_A_DLPFCFG_460HZ);
 }
 
-
-const c_shell_context = "SHELL";
+var c_shell_context = "SHELL";
 threadlocal var g_data_buf = [_]u8{0} ** 32;
 var g_streaming_imu = false;
 var g_raw = false;
-
-extern fn handle_rx_bytes([*c]const c.nrfx_uart_xfer_evt_t) void;
 
 export fn uart_event_handler(p_event: [*c]const c.nrfx_uart_event_t, p_context: ?*c_void) void {
     switch (p_event.*.type) {
@@ -55,6 +52,133 @@ export fn uart_event_handler(p_event: [*c]const c.nrfx_uart_event_t, p_context: 
     }
 }
 
-test "basic add functionality" {
-    testing.expect(add(3, 7) == 10);
+fn handle_rx_bytes(p_rxtx: *const c.nrfx_uart_xfer_evt_t) void {
+    switch (p_rxtx.p_data) {
+        'h' => {
+            const hello = "Hello World\r\n";
+            _ = c.nrfx_uart_tx(&g_uart0, hello, hello.len);
+            return;
+        },
+        else => return,
+    }
+}
+
+// switch(*p_rxtx->p_data){
+//     case 'h':{
+//         nrfx_uart_tx(&g_uart0, (uint8_t const *)"Hello World\r\n", sizeof("Hello World\r\n"));
+//         break;
+//     }
+//     case 's': {
+//         int32_t sum;
+//         sum = add(40, 2);
+//         char buf[32];
+//         sprintf((char *)buf, "Sum: %ld\r\n", sum);
+//         nrfx_uart_tx(&g_uart0, (uint8_t const *)buf, strlen(buf) + 1);
+//         break;
+//     }
+//     case 'i':{
+//         if(true == g_streaming_imu){
+//             g_streaming_imu = false;
+//         }else{
+//             trigger_imu_stream();
+//             g_streaming_imu = true;
+//             g_raw = false;
+//         }
+//         break;
+//     }
+
+//     case 'r':{
+//         if(true == g_streaming_imu){
+//             g_streaming_imu = false;
+//         }else{
+//             trigger_imu_stream();
+//             g_streaming_imu = true;
+//             g_raw = true;
+//         }
+//         break;
+//     }
+
+//     case 'l':{
+//         const char led_on_msg[] = "Turning on LED 0.\r\n";
+//         nrfx_uart_tx(&g_uart0, (uint8_t const *)led_on_msg, sizeof(led_on_msg));
+//         ledctrl_onoff(true, 0);
+
+//         break;
+//     }
+
+//     case 'k':{
+//         const char led_on_msg[] = "Turning off LED 0.\r\n";
+//         nrfx_uart_tx(&g_uart0, (uint8_t const *)led_on_msg, sizeof(led_on_msg));
+//         ledctrl_onoff(false, 0);
+
+//         break;
+//     }
+
+//     case 't':{
+//         if(false ==ledctrl_is_led_num_on(0)){
+//             const char led_on_msg[] = "Turning on LED 0.\r\n";
+//             nrfx_uart_tx(&g_uart0, (uint8_t const *)led_on_msg, sizeof(led_on_msg));
+//             ledctrl_onoff(true, 0);
+//         }else{
+//             const char led_off_msg[] = "Turning off LED 0.\r\n";
+//             nrfx_uart_tx(&g_uart0, (uint8_t const *)led_off_msg, sizeof(led_off_msg));
+//             ledctrl_onoff(false, 0);
+//         }
+//         break;
+//     }
+
+//     default:{
+//         nrfx_uart_tx(&g_uart0, (uint8_t const *)c_menu, sizeof(c_menu));
+//         break;
+//     }
+// }
+
+export fn shell_init() void {
+    const uart_cfg = c.nrfx_uart_config_t{
+        .pseltxd = 6,
+        .pselrxd = 8,
+        .pselcts = c.NRF_UART_PSEL_DISCONNECTED,
+        .pselrts = c.NRF_UART_PSEL_DISCONNECTED,
+        .p_context = @ptrCast(*c_void, &c_shell_context),
+        .hwfc = c.nrf_uart_hwfc_t.NRF_UART_HWFC_ENABLED,
+        .parity = c.nrf_uart_parity_t.NRF_UART_PARITY_EXCLUDED,
+        .baudrate = c.nrf_uart_baudrate_t.NRF_UART_BAUDRATE_115200,
+        .interrupt_priority = 7,
+    };
+    _ = c.nrfx_uart_init(&g_uart0, &uart_cfg, uart_event_handler);
+    _ = c.nrfx_uart_rx_enable(&g_uart0);
+    _ = c.nrfx_uart_rx(&g_uart0, &g_data_buf[0], 1);
+
+    const cfg = c.nrfx_timer_config_t{
+        .frequency = c.nrf_timer_frequency_t.NRF_TIMER_FREQ_16MHz,
+        .mode = c.nrf_timer_mode_t.NRF_TIMER_MODE_TIMER,
+        .bit_width = c.nrf_timer_bit_width_t.NRF_TIMER_BIT_WIDTH_32,
+        .interrupt_priority = 6,
+        .p_context = null,
+    };
+    _ = c.nrfx_timer_init(&g_polling, &cfg, polling_timer_event_handler);
+    const cc_time: u32 = c.nrfx_timer_ms_to_ticks(&g_polling, 10);
+    _ = c.nrfx_timer_extended_compare(&g_polling, c.nrf_timer_cc_channel_t.NRF_TIMER_CC_CHANNEL0, cc_time, c.nrf_timer_short_mask_t.NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK, true);
+    _ = c.nrfx_timer_enable(&g_polling);
+}
+
+export fn polling_timer_event_handler(event_type: c.nrf_timer_event_t, p_context: ?*c_void) void {
+    switch (event_type) {
+        c.nrf_timer_event_t.NRF_TIMER_EVENT_COMPARE0 => {
+            if (true == g_streaming_imu) { //check if we're streaming IMU data
+                // uint8_t accel_buf[6];
+                // mpu9250_drv_read_accel(&accel_buf);
+                // if(true == g_raw){
+                //     nrfx_uart_tx(&g_uart0, (uint8_t const *)accel_buf, sizeof(accel_buf));
+                // }else{
+                //     MPU9250_accel_val accel_val;
+                //     mpu9250_drv_process_raw_accel(&accel_val, &accel_buf);
+                //     char buf[64];
+                //     size_t len = sprintf((char *)buf, "ACCEL(x,y,z):%f,%f,%f\r\n",accel_val.x, accel_val.y, accel_val.z);
+                //     nrfx_uart_tx(&g_uart0, (uint8_t const *)buf, len);
+                // }
+            }
+        },
+        else => return,
+    }
 }
